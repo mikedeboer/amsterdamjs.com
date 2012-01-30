4112ec33285970187806718848e96d716f9d7502
mikedeboer <info@mikedeboer.nl>
2012-01-30 23:07:02 +0100

    temp server

---
+++ server.js
@@ -0,0 +1,6 @@
+var Http = require("http");
+
+Http.createServer(function(req, res) {
+    res.writeHead(200, {"content-type": "text/html"});s
+    res.end("<!doctype html><html><head><title>AmsterdamJS</title></head><body>Coming soon!</body></html>");
+}).listen(process.env.PORT);
